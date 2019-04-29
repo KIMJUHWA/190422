8fdb772e9ce123d2dabd0196ba70da904400b512
GD3 <GD3@DESKTOP-U50CULR>
2019-04-29 10:21:25 +0900

    배열

--- src/java0426/Main3.java
+++ src/java0426/Main3.java
@@ -2,7 +2,7 @@ package java0426;
 
 public class Main3 {
 
-	public static void main(String[] args) {
+	public  void main(String[] args) {
 		반복문3 while문 = new 반복문3();
 //		while문.t1();
 //		while문.t2();
